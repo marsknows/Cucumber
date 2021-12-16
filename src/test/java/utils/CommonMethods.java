900ba495b6b48d4934aa0e9cf58e9ce77977289e
marsknows <88752025+marsknows@users.noreply.github.com>
2021-12-15 18:35:06 -0600

    headless

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -24,10 +24,15 @@ public class CommonMethods {
         switch (ConfigReader.getPropertyValue("browser")) {
             case "chrome":
               //  System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.setHeadless(true);
-               WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver(chromeOptions);
+                if (ConfigReader.getPropertyValue("headless").equals("true")) {
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.setHeadless(true);
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(chromeOptions);
+                }else {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
                 break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
